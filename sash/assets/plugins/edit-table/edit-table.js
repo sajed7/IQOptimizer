5e0b987da23d83c55ee2c54aa2b1eb1c3ecf4896
Mo <52931433+mscybr@users.noreply.github.com>
2023-04-20 02:39:49 +0200

    Update edit-table.js

--- sash/assets/plugins/edit-table/edit-table.js
+++ sash/assets/plugins/edit-table/edit-table.js
@@ -1,3 +1,5 @@
+let Deleted_rows_table_plans = []
+let Deleted_rows_table_tags = []
 $(document).ready(function() {
 
     // Basic example
@@ -6,12 +8,36 @@ $(document).ready(function() {
 
     // New row edit-table example
     var example2 = new BSTable("new-edit", {
+        editableColumns:"0,1,2,3,4,5",
+        field_name: "subscription_plan",
         $addButton: $('#table2-new-row-button'),
         onEdit:function() {
-            console.log("EDITED");
         },
+        onBeforeDelete:($row)=>{
+            if ($row.children().last().prev().get()[0].firstChild) {
+                
+                Deleted_rows_table_plans.push($row.children().last().prev().get()[0].firstChild.getAttribute("value"))
+            }
+            console.log($row);
+        }
     });
     example2.init();
+    var example3 = new BSTable("new-edit2", {
+        editableColumns:"0",
+        field_name: "tag",
+        $addButton: $('#table3-new-row-button'),
+        onEdit:function() {
+        },
+        onBeforeDelete:($row)=>{
+            if ($row.children().last().prev().get()[0].firstChild) {
+                
+                Deleted_rows_table_tags.push($row.children().last().prev().get()[0].firstChild.getAttribute("value"))
+
+            }
+            console.log($row);
+        }
+    });
+    example3.init();
 
     // Example only some columns editable & removed actions column label
     var example3 = new BSTable("removecolumns-edit", {
@@ -24,3 +50,5 @@ $(document).ready(function() {
 
    
 } );
+
+
